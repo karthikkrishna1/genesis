11c059283f1cbe30ad7255af643f499d468e02fd
Karthik Krishna <karthik4kittu@gmail.com>
2024-03-30 17:50:42 -0400

    Fixed dependency errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,15 @@ import "./App.css";
 import LobbyScreen from "./screens/Lobby";
 import RoomPage from "./screens/Room";
 import { React, useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Switch,
+} from "react-router-dom";
 import axios from "axios";
-import {format} from "date-fns";
-import './App.css';
+import { format } from "date-fns";
+import "./App.css";
 import Home from "./pages/home";
 import Login from "./pages/login";
 
@@ -15,16 +20,11 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        
+        <Route exact path="/" element={<Home />} />
+        {/* <Route exact path="/call" element={<Call />} /> */}
+        <Route path="/" element={<LobbyScreen />} />
+        <Route path="/room/:roomId" element={<RoomPage />} />
       </Routes>
-        <Router>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            {/* <Route exact path="/call" element={<Call />} /> */}
-            <Route path="/" element={<LobbyScreen />} />
-            <Route path="/room/:roomId" element={<RoomPage />} />
-          </Routes>
-        </Router>
     </div>
   );
 }
